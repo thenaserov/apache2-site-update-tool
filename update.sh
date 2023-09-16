f4789fec117d598df639f4b246848475802e9f8b
naserrezayi1998 <naser.rezayi.nr@gmail.com>
2023-09-16 09:42:10 +0330

    [ADD] rem files first then update

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo rm -rf css
+sudo rm -rf img
+sudo rm -rf js
+sudo rm -rf tools
+
+sudo rm index.html
+sudo rm product.html
+sudo rm search.html
+sudo rm 404.html
+
+
 sudo cp online-tshirt-website/index.html ../
 sudo cp online-tshirt-website/404.html ../
 sudo cp online-tshirt-website/product.html ../
